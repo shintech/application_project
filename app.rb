b40a75cbe4dd14534ff64bbac36af37eb16a003e
Michael Prather <mprather1@gmail.com>
2016-09-03 23:35:59 +0000

    error handling and readme update

--- app.rb
+++ app.rb
@@ -16,20 +16,34 @@ class Scraper
   
   def xml_to_redis
     urls = []
+    retries = 3
     page = Nokogiri::HTML(open(@target))
     page.xpath('//a/@href').each do |links|
       urls << links.content
     end
     puts "Downloading files from #{@target}..."
     urls.each do |url|
-      if url.split(".").last == "zip"
-        download = open("#{@target}#{url}")
-        Zip::File.open(download) do |zip_file|
-          zip_file.each do |f|
-            xmldoc = f.get_input_stream.read
-            @r.sadd "#{@redis_list}", "#{xmldoc}"
+      begin
+        if url.split(".").last == "zip"
+          download = open("#{@target}#{url}")
+          Zip::File.open(download) do |zip_file|
+            zip_file.each do |f|
+              xmldoc = f.get_input_stream.read
+              @r.sadd "#{@redis_list}", "#{xmldoc}"
+            end
+            puts "Finished processing #{url}..."
           end
-          puts "Finished processing #{url}..."
+        end
+      rescue StandardError => e
+        if retries == 0
+          puts "Error skipped after three failed attempts..."
+          File.write("./log.txt", "Skipped #{@target}#{url} at #{DateTime.now}")
+          retries = 3
+          next
+        else
+          puts "Error retrying..."
+          retries -= 1
+          retry
         end
       end
     end
--- readme.txt
+++ readme.txt
@@ -7,8 +7,7 @@ Requires Ruby Gems listed below:
   "redis"
   "fileutils"
 
-Downloads files linked to in <a> tags on url specified in Scraper.target to .bin/download_directory.
-Imports contents of zip files in download directory to Redis list entitled 'NEWS_XML'.
+Downloads files linked to in <a> tags on url specified in Scraper.target and imports contents of zip files to Redis list specified in Scraper.redis_list.
 
 Target url and Redis list can be changed as per instructions below.
 
@@ -21,5 +20,4 @@ Open terminal in directory and type "ruby run.rb"
 app.target("http://example.org/whatever")    # url format => "http://example.org/whatever/"
 app.redis_list("NEWS_XML")    # name of Redis list.
 app.config_directory    # creates download directory with a unique name.
-app.download_files    # downloads the files to bin/folder.download.
-app.xml_to_redis     # extracts contents of zip folders to redis_list
+app.xml_to_redis     # downloads and extracts contents of zip folders to redis_list
